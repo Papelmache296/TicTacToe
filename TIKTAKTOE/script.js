81f43100477ffa1ba81d431f9e52dfd6d1cdd10a
Papelmache296 <2024296@student.cct.ie>
2025-04-04 15:43:15 +0100

    Code
    Base code was added

---
+++ TIKTAKTOE/script.js
@@ -0,0 +1,6 @@
+
+// event listener para que el mouse cambie el estado de la celda (para cada celda)
+// ecent listener para que no se pueda volver a usar la misma celda
+// event  para hacer check si alguien gana
+// event para hacer check si empatan
+// event lostener para pasar al siguiente turno
